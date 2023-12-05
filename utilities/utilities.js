df13daa49e747a6d9acfdd77d1195401fc8e4770
Null-Cat <20419682+Null-Cat@users.noreply.github.com>
2023-12-05 02:39:32 +0000

    Bugfix Race Condition on Session Authentication

--- utilities/utilities.js
+++ utilities/utilities.js
@@ -41,6 +41,10 @@ function authenticateJWT(req, res, next) {
               if (rows.length == 0) {
                 console.log(`${logTimestamp} ${clc.bold(user.username)} Session ${clc.red('Expired')}`)
                 return res.sendStatus(401)
+              } else {
+                console.log(`${logTimestamp} ${clc.bold(user.username)} ${clc.green('Authenticated')}`)
+                req.user = user
+                next()
               }
               conn.end()
             })
@@ -55,54 +59,52 @@ function authenticateJWT(req, res, next) {
           console.log(err)
           res.sendStatus(500)
         })
-      console.log(`${logTimestamp} ${clc.bold(user.username)} ${clc.green('Authenticated')}`)
-      req.user = user
-      next()
     })
   } else {
     console.log(`${logTimestamp} ${clc.red('Not Authenticated')}`)
     res.sendStatus(401)
+    return
   }
 }
 
 async function hasPerms(requiredPerms, user) {
-  return pr = new Promise(function(resolve, reject) {
-  mariadbPool.pool
-    .getConnection()
-    .then((conn) => {
-      conn
-        .query('SELECT perms FROM players WHERE username = ?', [user.username])
-        .then((rows) => {
-          if (rows.length == 0) {
-            console.log(`${logTimestamp} ${clc.red('Invalid')} Username`)
-            resolve(false)
-            conn.end()
-            return
-          } else {
-            var perms = rows[0].perms
-            for (var i = 0; i < requiredPerms.length; i++) {
-              if (!perms.includes(requiredPerms[i])) {
-                console.log(`${logTimestamp} User ${clc.bold(user.username)} has ${clc.red('Invalid')} ${requiredPerms.join(', ')} Permissions`)
-                resolve(false)
-                conn.end()
-                return
+  return (pr = new Promise(function (resolve, reject) {
+    mariadbPool.pool
+      .getConnection()
+      .then((conn) => {
+        conn
+          .query('SELECT perms FROM players WHERE username = ?', [user.username])
+          .then((rows) => {
+            if (rows.length == 0) {
+              console.log(`${logTimestamp} ${clc.red('Invalid')} Username`)
+              resolve(false)
+              conn.end()
+              return
+            } else {
+              var perms = rows[0].perms
+              for (var i = 0; i < requiredPerms.length; i++) {
+                if (!perms.includes(requiredPerms[i])) {
+                  console.log(`${logTimestamp} User ${clc.bold(user.username)} has ${clc.red('Invalid')} ${requiredPerms.join(', ')} Permissions`)
+                  resolve(false)
+                  conn.end()
+                  return
+                }
               }
+              console.log(`${logTimestamp} User ${user.username} has ${clc.green('Valid')} ${requiredPerms.join(', ')} Permissions`)
+              resolve(true)
+              conn.end()
+              return
             }
-            console.log(`${logTimestamp} User ${user.username} has ${clc.green('Valid')} ${requiredPerms.join(', ')} Permissions`)
-            resolve(true)
+          })
+          .catch((err) => {
+            //handle error
+            console.log(err)
             conn.end()
-            return
-          }
-        })
-        .catch((err) => {
-          //handle error
-          console.log(err)
-          conn.end()
-        })
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-})
+          })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }))
 }
 module.exports = { logTimestamp, getIP, authenticateJWT, hasPerms }
